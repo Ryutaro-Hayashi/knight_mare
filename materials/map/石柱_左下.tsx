bde3edf92ab213c8aa8168e5fcc993dad039b3fb
Ryutaro-Hayashi <rimda21@gmail.com>
2020-10-27 23:18:31 +0900

    first commit

---
+++ materials/map/石柱_左下.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="石柱_左下" tilewidth="8" tileheight="8" tilecount="4" columns="2">
+ <image source="石柱_左下.bmp" width="16" height="16"/>
+</tileset>
